81a7ce209c2ebba09d94f2298563d11340481340
Artem <merkushynwrite@gmail.com>
2023-03-06 18:11:27 +0200

    vercel

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'http://localhost:3002/api',
+   //baseURL: 'http://localhost:3002/api',
+   baseURL: 'https://rental-property-api.vercel.app/api',
 });
 
 instance.interceptors.request.use((config) => {
